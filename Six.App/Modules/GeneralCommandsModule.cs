3a3c633fd2d43f6530139b30ebd5f7cf49b04be3
Vast <Vastlee@gmail.com>
2023-06-29 14:11:48 -0700

    Created Dev Branch to serve as buffer to release.#

--- Six.App/Modules/GeneralCommandsModule.cs
+++ Six.App/Modules/GeneralCommandsModule.cs
@@ -6,6 +6,7 @@ public class GeneralCommandsModule : ModuleBase<SocketCommandContext> {
     [Summary("Responds with the Lifting Day!")]
     public Task DYELReplyAsync(int offset = 0) {
         WorkoutDay todaysWorkout = DYEL.GetWorkout(offset);
+        // Example: if(Context.User.Id == 284370066873647105) { DoTheSpecialRobbyStuff(); }
         return ReplyAsync($"Damn Right I Do! Today's focus is {todaysWorkout.Type} Round #{todaysWorkout.Round}");
     }
 }
